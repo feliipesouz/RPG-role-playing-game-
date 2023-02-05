e11b93d42ff6567a150d54dc1fbecfb43fb7fb70
FELIPE ARRUDA DE SOUZA <felipe_rhnossa@3coracoes.com.br>
2023-02-05 20:13:40 -0300

    Move completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,41 @@
+import React from "react";
 import { Character } from "./components/Character";
+import { useCharacter } from "./hooks/useCharacter";
 import { Container, Map } from "./styles";
 
 const App = () => {
+  const useChar = useCharacter();
+
+  React.useEffect(() => {
+    window.addEventListener("keydown", getKey);
+  }, []);
+
+  const getKey = (e: KeyboardEvent) => {
+    console.log(e.code);
+    switch (e.code) {
+      case "KeyA":
+      case "ArrowLeft":
+        useChar.moveLeft();
+        break;
+      case "KeyW":
+      case "ArrowUp":
+        useChar.moveUp();
+        break;
+      case "KeyD":
+      case "ArrowRight":
+        useChar.moveRight();
+        break;
+      case "KeyS":
+      case "ArrowDown":
+        useChar.moveDown();
+        break;
+    }
+  };
+
   return (
     <Container>
       <Map>
-        <Character x={5} y={0} />
+        <Character x={useChar.x} y={useChar.y} />
       </Map>
     </Container>
   );
--- src/hooks/useCharacter.ts
+++ src/hooks/useCharacter.ts
@@ -1,3 +1,43 @@
 //Usamos ts nos hooks pois eles não entregam nada gráfico, somente propriedades.
+import React from "react";
+export const useCharacter = () => {
+  const [pos, setPos] = React.useState({ x: 3, y: 5 });
 
-export const useCharacter = () => {};
+  const moveLeft = () => {
+    setPos((pos) => ({
+      x: pos.x - 1,
+      y: pos.y,
+    }));
+  };
+
+  const moveRight = () => {
+    setPos((pos) => ({
+      x: pos.x + 1,
+      y: pos.y,
+    }));
+  };
+
+  const moveUp = () => {
+    setPos((pos) => ({
+      x: pos.x,
+      y: pos.y - 1,
+    }));
+  };
+
+  const moveDown = () => {
+    setPos((pos) => ({
+      x: pos.x,
+      y: pos.y + 1,
+    }));
+  };
+
+  //Aqui retornamos um obj com as propriedades que queremos dar acesso a quem tiver manipulando o nosso boneco
+  return {
+    x: pos.x,
+    y: pos.y,
+    moveLeft,
+    moveRight,
+    moveUp,
+    moveDown,
+  };
+};
